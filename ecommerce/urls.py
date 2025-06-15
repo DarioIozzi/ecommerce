c037b51b2a5b5974e6e5f933c71b6b14e7f50d16
Dario <dario.iozzi@edu.unifi.it>
2025-06-15 19:10:34 +0200

    Aggiunta app registration

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from registration import views as registration_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('registration/', registration_views.registrationView, name="registration"),
+    path('', include('store.views'))
 ]
---
+++ registration/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+
+# Create your views here.
+def registrationView(response):
+    if response.method == "POST":
+        form = RegistrationForm(response.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/login")
+        else:
+            form = RegistrationForm()
+
+    return render(response, 'registration/registration.html', {"form" : form})
