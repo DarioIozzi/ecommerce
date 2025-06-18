06091f11d72c1539afa2127b99129c1f644d125f
Dario <dario.iozzi@edu.unifi.it>
2025-06-18 15:46:08 +0200

    Aggiunta app store

---
+++ store/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path , include
+from . import views
+
+urlpatterns = [
+    path("", views.home),
+
+    path("home/", views.home, name="home"),
+
+    path("cliente/", views.cliente, name="cliente"),
+
+    path("aggiungiProdotto/", views.aggiungiProdotto, name="aggiungiProdotto"),
+
+    path("gestioneProdotti/", views.gestioneProdotti, name="gestioneProdotti"),
+
+    path("gestioneAcquisto/", views.gestioneAcquisto, name="gestioneAcquisto"),
+
+    path("aggiungiAlCarrello/", views.aggiungiAlCarrello, name="aggiungiAlCarrello"),
+
+    path("effettuaOrdine/", views.effettuaOrdine, name="effettuaOrdine"),
+
+    path("revisioneOrdine/", views.revisioneOrdine, name="revisioneOrdine"),
+
+    path("gestioneCarrello/", views.gestioneCarrello, name="gestioneCarrello"),
+
+    path("salvaPagamento/", views.salvaPagamento, name="salvaPagamento"),
+
+    path("vediOrdine/", views.vediOrdine, name="vediOrdine"),
+
+    path("cronologiaOrdini/", views.cronologiaOrdini, name="cronologiaOrdini"),
+]
