2ee3ec3315f10cfc7831c06d3a94c35a26b42438
Dario <dario.iozzi@edu.unifi.it>
2025-06-20 18:43:53 +0200

    Aggiunti fogli di stile, svg e js

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/', registration_views.registrationView, name="registration"),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('store.urls')),
     path('', include("django.contrib.auth.urls")),
 ]
